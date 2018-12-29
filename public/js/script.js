885655d5b0050adccf5b286e9705fd4e2f2ba0d7
Suraj Adhikari <heartbreakkidhbk97@gmail.com>
2018-12-29 15:10:26 +0545

    added newMessage & createMessage

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,28 @@
 var socket = io();
-socket.on("connect", () => {
+socket.on("connect", function() {
   console.log("Connected to server..");
+
+  socket.emit("createEmail", {
+    to: "asuraj20@yahoo.com",
+    text: "Interesting"
+  });
+
+  socket.emit("createMessage", {
+    from: "Asmita Lammichane",
+    text: "Create Your First Message"
+  });
 });
 
-socket.on("disconnect", () => {
+socket.on("disconnect", function() {
   console.log("Disconnected from server..");
+});
+
+socket.on("newEmail", function(email) {
+  //data = from text cA
+  //Custom EventListener
+  console.log("New Email Received", email);
+});
+
+socket.on("newMessage", function(message) {
+  console.log("New Message Received", message);
 });
--- server/server.js
+++ server/server.js
@@ -8,15 +8,37 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, "../public")));
+//Connection
 io.on("connection", socket => {
   console.log("New user connected");
 
+  socket.emit("newEmail", {
+    from: "asuraj893@gmail.com",
+    text: "Hello socket.io",
+    createAt: 14 / 09 / 2075
+  });
+
+  socket.emit("newMessage", {
+    from: "Asmita Lamichane",
+    text: "Your first message using socket.io",
+    createdAt: 14 / 09 / 2075
+  });
+
+  socket.on("createEmail", newEmail => {
+    console.log("Create New Email", newEmail);
+  });
+
+  socket.on("createMessage", newMessage => {
+    console.log("Create New Message", newMessage);
+  });
+
+  //Disconnect
   socket.on("disconnect", () => {
     console.log("User Disconnected..");
   });
 });
-
-app.use(express.static(path.join(__dirname, "../public")));
 
 server.listen(port, () => {
   console.log(`Server Started on ${port}`);
